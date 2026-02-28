81885624df9c9c9fb1beb4cdaca92729f9ae4423
Erik Wiffin <erik.wiffin@gmail.com>
2026-02-28 11:16:38 -0500

    Codegen uses backend schema

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,8 +1,12 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import { loadEnv } from "vite";
+
+const mode = process.env.MODE ?? process.env.NODE_ENV ?? "development";
+const env = loadEnv(mode, process.cwd());
 
 const config: CodegenConfig = {
-  schema: "src/graphql/schema.graphql",
-  documents: ["src/**/*.graphql", "!src/graphql/schema.graphql"],
+  schema: env.VITE_GRAPHQL_HTTP_URL,
+  documents: ["src/**/*.graphql"],
   generates: {
     "src/__generated__/": {
       preset: "client",
